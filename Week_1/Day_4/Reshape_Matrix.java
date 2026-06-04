9d405a84450bc8ae64cb5a0089eb7fd537400eb9
Sagar Patel <24115cn032@glbitm.ac.in>
2026-06-05 01:10:12 +0530

    Refactor matrix reshape logic for clarity

--- Week_1/Day_4/Reshape_Matrix.java
+++ Week_1/Day_4/Reshape_Matrix.java
@@ -1,22 +1,20 @@
 class Reshape_Matrix {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-       int rows = mat.length;
-       int cols = mat[0].length;
-       if((rows * cols) != (r * c)) return mat;
-
-       int[][] output=new int[r][c];
-       int col=0;
-       int row=0;
-       for(int i=0; i<rows; i++){
-        for(int j=0; j<cols; j++){
-            output[row][col]=mat[i][j];
-            col++;
-            if(col == c){
-                col=0;
-                row++;
+       if( (mat.length * mat[0].length) != r*c ){
+            return mat;
+        }
+        int[][] arr = new int[r][c];
+        int m = 0, n = 0;
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                arr[i][j] = mat[m][n];
+                n++;
+                if(n > mat[0].length-1){
+                    n = 0;
+                    m++;
+                }
             }
         }
-       } 
-       return output;
+        return arr;
     }
 }
