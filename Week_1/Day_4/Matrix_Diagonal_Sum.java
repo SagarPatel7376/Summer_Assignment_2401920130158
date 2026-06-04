e722a7d90b71c0bbd412d034cf045b5bdacf8177
Sagar Patel <24115cn032@glbitm.ac.in>
2026-06-04 23:07:32 +0530

    Refactor class name and variable initialization

--- Week_1/Day_4/Matrix_Diagonal_Sum.java
+++ Week_1/Day_4/Matrix_Diagonal_Sum.java
@@ -1,10 +1,10 @@
-class Solution {
+class Matrix_Diagonal_Sum {
     public int diagonalSum(int[][] mat) {
-        int sum =0;
-        for(int i=0;i<mat.length;i++){
+        int sum = 0;
+        for(int i = 0;i < mat.length; i++){
             sum += mat[i][i];
-            if(i != mat.length -1 -i){ 
-                sum += mat[i][mat.length -1 -i];
+            if(i != mat.length-1-i){ 
+                sum += mat[i][mat.length-1-i];
             }
         }
         return sum;
