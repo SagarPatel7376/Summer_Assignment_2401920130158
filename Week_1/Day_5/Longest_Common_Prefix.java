a00eb3099c82ad2c988bab9a38f51b98863c5210
Sagar Patel <24115cn032@glbitm.ac.in>
2026-06-06 01:18:47 +0530

    Fix longestCommonPrefix method logic and variable names

--- Week_1/Day_5/Longest_Common_Prefix.java
+++ Week_1/Day_5/Longest_Common_Prefix.java
@@ -1,15 +1,14 @@
 class Longest_Common_Prefix {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0){
-          return "";
+            return "";
         }
-        String prefix = str[0];
-        for (int i = 1; i < str.length; i++) {
-            while (!str[i].startsWith(prefix)) {
+        
+        String prefix = strs[0];
+        
+        for (String s : strs) {
+            while (s.indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()){
-                  return "";
-                }
             }
         }
         return prefix;
