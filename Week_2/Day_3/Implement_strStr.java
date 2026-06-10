f0c8b0db6bcde4b72dfb8d799d89eccc062344f7
Sagar Patel <24115cn032@glbitm.ac.in>
2026-06-10 22:58:35 +0530

    Implement strStr method for substring search

---
+++ Week_2/Day_3/Implement_strStr.java
@@ -0,0 +1,12 @@
+class Implement_strStr {
+    public int strStr(String haystack, String needle) {
+         for (int i = 0; i < haystack.length()-needle.length()+1; i++) {
+            if(needle.charAt(0) == haystack.charAt(i)){
+                if(haystack.substring(i, i+needle.length()).equals(needle)){
+                    return i;
+                }
+            }
+        }
+         return -1;
+    }
+}
